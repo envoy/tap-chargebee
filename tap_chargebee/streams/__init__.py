0cf75f88163ceabb7af592bd937b4b1397f2e38b
David Wallace <dwallace0723@gmail.com>
2019-04-06 13:53:10 -0700

    add virtual bank account stream to available streams

--- tap_chargebee/streams/__init__.py
+++ tap_chargebee/streams/__init__.py
@@ -7,6 +7,7 @@ from .payment_sources import PaymentSourcesStream
 from .plans import PlansStream
 from .subscriptions import SubscriptionsStream
 from .transactions import TransactionsStream
+from .virtual_bank_accounts import VirtualBankAccountsStream
 
 AVAILABLE_STREAMS = [
     AddonsStream,
@@ -17,5 +18,6 @@ AVAILABLE_STREAMS = [
     PlansStream,
     SubscriptionsStream,
     TransactionsStream,
-    EventsStream
+    EventsStream,
+    VirtualBankAccountsStream
 ]
