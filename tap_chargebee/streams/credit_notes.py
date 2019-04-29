c7e834ea032f0db8a63bc1f941d72897ecf3fb53
David Wallace <dwallace0723@gmail.com>
2019-04-29 10:28:43 -0700

    add credit notes stream

---
+++ tap_chargebee/streams/credit_notes.py
@@ -0,0 +1,16 @@
+from tap_chargebee.streams.base import BaseChargebeeStream
+
+
+class CreditNotesStream(BaseChargebeeStream):
+    TABLE = 'credit_notes'
+    ENTITY = 'credit_note'
+    KEY_PROPERTIES = ['id']
+    BOOKMARK_PROPERTIES = ['updated_at']
+    SELECTED_BY_DEFAULT = True
+    VALID_REPLICATION_KEYS = ['updated_at']
+    INCLUSION = 'available'
+    SELECTED = True
+    API_METHOD = 'GET'
+
+    def get_url(self):
+        return 'https://{}.chargebee.com/api/v2/credit_notes'.format(self.config.get('site'))
