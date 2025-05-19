23d4578a2884396adf9d05e58e48fa2e00c4c447
gl-romil <129256325+gl-romil@users.noreply.github.com>
2025-05-19 17:08:53 +0530

    SAC-27624: update dependencies in setup.py to latest versions

--- setup.py
+++ setup.py
@@ -2,25 +2,34 @@
 
 from setuptools import setup, find_packages
 
-setup(name='tap-chargebee',
-      version='1.4.0',
-      description='Singer.io tap for extracting data from the Chargebee API',
-      author='dwallace@envoy.com',
-      classifiers=['Programming Language :: Python :: 3 :: Only'],
-      py_modules=['tap_chargebee'],
-      install_requires=[
-          'tap-framework==0.1.1'
-      ],
-      entry_points='''
-          [console_scripts]
-          tap-chargebee=tap_chargebee:main
-      ''',
-      packages=find_packages(),
-      package_data={
-          'tap_chargebee': [
-              'schemas/common/*.json',
-              'schemas/item_model/*.json',
-              'schemas/plan_model/*.json'
-          ]
-      },
-      include_package_data=True)
+setup(
+    name='tap-chargebee',
+    version='1.4.0',
+    description='Singer.io tap for extracting data from the Chargebee API',
+    author='dwallace@envoy.com',
+    classifiers=[
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+    py_modules=['tap_chargebee'],
+    install_requires=[
+        'tap-framework==0.1.1',
+        'singer-python==6.1.1',
+        'requests==2.32.3',
+        'backoff==2.2.1',
+        'simplejson==3.20.1'
+    ],
+    entry_points='''
+        [console_scripts]
+        tap-chargebee=tap_chargebee:main
+    ''',
+    packages=find_packages(),
+    package_data={
+        'tap_chargebee': [
+            'schemas/common/*.json',
+            'schemas/item_model/*.json',
+            'schemas/plan_model/*.json',
+            'schemas/*.json'
+        ]
+    },
+    include_package_data=True
+)
